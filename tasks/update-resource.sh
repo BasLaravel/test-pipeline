598bf47cfc48ad4f71466ec52e6ab4fccb78adce
Sebastiaan Kingma <sebastiaankingma@sebastiaans-mbp.groningen.nl.ibm.com>
2019-02-12 16:34:55 +0100

    First commit

--- tasks/update-resource.sh
+++ tasks/update-resource.sh
@@ -9,11 +9,10 @@ ls
 # git clone resource-gist updated-gist
 
 # cd updated-gist
-echo $(date) > first-gist
+echo "asdfsdfsd" > first-gist
 
 git config --global user.email "bazzkingma@hotmail.com"
 git config --global user.name "BasLaravel"
 
 git add .
 git commit -m "Bumped date"
-git push
